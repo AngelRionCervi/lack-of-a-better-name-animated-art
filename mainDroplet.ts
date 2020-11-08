781fba43288ce62792f703231819237bfbfdfd18
AngelRionCervi <a.rioncervi@gmail.com>
2020-11-08 01:09:23 +0100

    init

--- mainDroplet.ts
+++ mainDroplet.ts
@@ -24,77 +24,24 @@ const cw: any = container.offsetWidth;
 const ch: any = container.offsetHeight;
 
 const baseRadius = 600;
-const waveDeepness = 0.28;
-const waveThreshold = 300;
-const skewDeg = -20;
-const rotationDeg = 45;
-const speed = 0.5;
-const zIndexOffset = 10;
 
 const getCenter = (radius: number) => {
     return [cw / 2 - radius / 2, ch / 2 - radius / 2];
 };
 
+
 [...circles].reverse().forEach((circle: any, index: number, arr: any) => {
-    circle.style.transform = `rotate(${rotationDeg}deg) skew(${skewDeg}deg, ${skewDeg}deg)`;
     const radius = Math.round(baseRadius * ((index + 0.5) / arr.length)); //Math.round((index + circles.length) * ((index+0.5) * (circles.length/50)));
     const [x, y] = getCenter(radius);
-    style(circle).left(`${x}px`).top(`${y}px`).width(`${radius}px`).height(`${radius}px`); //.zIndex(`${index}`); // looks good
+    style(circle).left(`${x}px`).top(`${y}px`).width(`${radius}px`).height(`${radius}px`);
 });
 
-circles.forEach((circle: any, index: number, arr: any) => {
-    // const top = parseInt(circle.style.top.replace("px", "")) - index * 11;
-    // style(circle).top(`${top}px`);
-    // if (index < 10) {
-    //     const top = parseInt(circle.style.top.replace("px", "")) - index * 11;
-    //     style(circle).top(`${top}px`);
-    // } else {
-    //     const top = parseInt(circle.style.top.replace("px", "")) - (arr.length - index) * 11;
-    //     style(circle).top(`${top}px`);
-    // }
+circles.forEach((circle: any, index: number) => {
+    const top = parseInt(circle.style.top.replace("px", "")) - index * 11;
+    style(circle).top(`${top}px`);
 });
 
-const justAZoom = () => {
-    const steps = Array.from({ length: circles.length }, (_, i) => i * 30);
-    const getTopOffset = (radius: number): number => {
-        if (radius > waveThreshold) {
-            return radius * waveDeepness;
-        }
-        return -radius * waveDeepness + waveDeepness * baseRadius;
-    };
 
-    const getZindex = (radius: number): number => {
-        const closest = steps.reduce((prev: any, curr: any) =>
-            Math.abs(curr - radius) < Math.abs(prev - radius) ? curr : prev
-        );
-        return (circles.length - steps.indexOf(closest) - 1) + zIndexOffset;
-    };
-
-    const zoomCircle = (circle: HTMLElement) => {
-        let radius = parseFloat(circle.style.width.replace("px", ""));
-        if (radius > baseRadius) {
-            radius = 1;
-        }
-
-        const [x, y] = getCenter(radius + speed);
-        const topOffset = getTopOffset(radius + speed);
-
-        style(circle)
-            .width(`${radius + speed}px`)
-            .height(`${radius + speed}px`)
-            .left(`${x}px`)
-            .top(`${y + topOffset}px`)
-            .opacity(`${(baseRadius - radius) / baseRadius}`)
-            .zIndex(`${getZindex(radius)}`);
-        requestAnimationFrame(() => zoomCircle(circle));
-    };
-
-    circles.forEach((circle: HTMLElement) => {
-        zoomCircle(circle);
-    });
-};
-
-justAZoom();
 
 // const oneByOne = (index: number, sign: string) => {
 //     anime({
@@ -115,9 +62,10 @@ justAZoom();
 // oneByOne(0, "+");
 
 const droplet = () => {
-    const circlesArr = [...circles].reverse();
-    const circlesToAnimate = [...circlesArr].splice(0, 16);
 
+    const circlesArr = [...circles].reverse();
+    const circlesToAnimate = [...circlesArr].splice(0, 16)
+    
     anime({
         targets: circlesToAnimate,
         delay: anime.stagger(50, { easing: "linear" }),
@@ -150,6 +98,7 @@ const droplet = () => {
             //         // circle.style.height = `${radius}px`;
             //         // circle.style.top = `${y}px`;
             //         // circle.style.left = `${x}px`;
+    
             //     } else {
             //         const [x, y] = getCenter(width);
             //         //style(circle).width(`${width + 1}px`).height(`${height + 1}px`).left(`${x}px`).top(`${y}px`);
@@ -160,6 +109,7 @@ const droplet = () => {
         loop: true,
         direction: "alternate",
     });
-};
+}
 
 //droplet();
+
